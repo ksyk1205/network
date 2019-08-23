2e12b53124168579c17248a6d9c46d4d5db512a8
BIT <BIT@DESKTOP-ET39DN4>
2019-08-23 17:33:05 +0900

    network

--- src/echo/EchoClient.java
+++ src/echo/EchoClient.java
@@ -10,72 +10,66 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoClient {
-	private static String SERVER_IP="192.168.1.13";
-	private static final int PORT =8000;
-
+	private static String SERVER_IP = "192.168.1.16";
+	private static int SERVER_PORT = 8000;
+	
 	public static void main(String[] args) {
-		Socket socket = null;
 		Scanner scanner = null;
-
+		Socket socket = null;
+	
 		try {
-			//1.scanner 생성(표준입력, 키보드 연결)
+			//1. Scanner 생성(표준입력, 키보드 연결)
 			scanner = new Scanner(System.in);
 			
-			//2.소켓생성
-			socket =new Socket();
+			//2. 소켓생성
+			socket = new Socket();
 			
-			//3.서버연결
-//			InetSocketAddress inetsocketAddress =new InetSocketAddress(SERVER_IP,PORT);
-			socket.connect(new InetSocketAddress(SERVER_IP,PORT));
+			//3. 서버연결
+			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
 			log("connected");
 			
-			//4.IOStream  생성
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"UTF-8"),true);
-			
-			
+			//4. I/O Stream생성
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+
 			while(true) {
-			//5.키보드 입력 받기 	
+				//5. 키보드 입력 받기
 				System.out.print(">>");
 				String line = scanner.nextLine();
-				
 				if("quit".equals(line)) {
 					break;
 				}
-				//6.데이터 쓰기(전송)
+
+				//6. 데이터 쓰기(전송)
 				pw.println(line);
 				
-				//7.데이터 읽기(수신)
+				//7. 데이터 읽기(수신)
 				String data = br.readLine();
-				if(data==null) {
+				if(data == null) {
 					log("closed by client");
 					break;
 				}
 				
-				//8.콘솔출력
-				System.out.println("<<"+data);
-
-
+				//8. 콘솔 출력
+				System.out.println("<<" + data);
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
 				if(scanner != null) {
 					scanner.close();
 				}
-				if(socket !=null && socket.isClosed() ==false) {
+				if(socket != null && socket.isClosed() == false) {
 					socket.close();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
-
+		}		
 	}
+	
 	private static void log(String log) {
-		System.out.println("[Echo Client]"+log);
+		System.out.println("[Echo Client] " + log);
 	}
-
 }
--- src/echo/EchoServer.java
+++ src/echo/EchoServer.java
@@ -1,76 +1,39 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
-
 
 public class EchoServer {
-	private static final int PORT =8000;
-
+	private static final int PORT = 8000;
+	
 	public static void main(String[] args) {
-		ServerSocket serverSocket =null;
+		ServerSocket serverSocket = null;
+		
 		try {
-			//1.서버소켓 생성
-			serverSocket= new ServerSocket();
+			//1. 서버소켓 생성
+			serverSocket = new ServerSocket();
 			
-			//2.Binding : Socket에 SocketAddress(IPAddress + Port) 바인딩한다
-			InetAddress inetAddress =InetAddress.getLocalHost();//ip주소를 얻어올 inetAddress
-			InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress,PORT);
-			serverSocket.bind(inetSocketAddress);//
-			log("binding "+inetAddress.getHostAddress()+":"+PORT);
-
-			//3. accept: 클라이언트로 부터 연결요청(Connect)을 기다린다.
-			Socket socket =serverSocket.accept();
-			InetSocketAddress inetRemoteSocketAddress =(InetSocketAddress) socket.getRemoteSocketAddress();
+			//2. Binding
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, PORT);
+			serverSocket.bind(inetSocketAddress);
+			log("binding " + inetAddress.getHostAddress() + ":" + PORT);
 			
-			log("connected from client["+inetRemoteSocketAddress.getAddress().getHostAddress()+":"+inetRemoteSocketAddress.getPort()+"]");
-			
-			try {
-				
-				//4. I/O Stream생성
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"UTF-8"),true);
-				
-				
-				while(true) {
-					//5.데이터 읽기 (수신)
-					String data =br.readLine();
-					if(data == null) {
-						log("closed by client");
-						break;
-					}
-					log("received:" +data);
-					
-					//6.데이터 쓰기 (전송)
-					pw.println(data);
-					
-				}
-
-			}catch(SocketException e) {
-
-				log("abnormal closed by client");
-
-			}catch(IOException e) {
-				e.printStackTrace();
-
-			}finally {
-				if(socket != null && socket.isClosed() ==false)
-					socket.close();
+			//3. accept
+			while(true) {
+				Socket socket = serverSocket.accept();
+				new EchoServerReceiveThread(socket).start();
 			}
-
+			
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
+			//8. Server Socket 자원정리
 			try {
-				if(serverSocket !=null && serverSocket.isClosed()==false) {
+				if(serverSocket != null && serverSocket.isClosed() == false) {
 					serverSocket.close();
 				}
 			} catch (IOException e) {
@@ -78,8 +41,8 @@ public class EchoServer {
 			}
 		}
 	}
-	public static void log(String log) {
-		System.out.println("[Echo Server]"+log);
-	}
 
+	public static void log(String log) {
+		System.out.println("[Echo Server#" + Thread.currentThread().getId() + "] " + log);
+	}
 }
